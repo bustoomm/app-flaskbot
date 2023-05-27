1cfddd4df38a46a06734b2f678a25a05f468f76f
unknown <admin@nl516-nb.nl.com>
2023-05-28 01:09:50 +0700

    fourth commit

---
+++ credentials.py
@@ -0,0 +1,17 @@
+# import tweepy
+
+consumer_key = "QlfUg1LqOhXQ0CeOZWCAktA59"
+consumer_secret = "OY5jKfIXnOdp76e5FNGG8cGVwmxgHzhFeruOAbulpRTHbryS2Z"
+bearer_token = "AAAAAAAAAAAAAAAAAAAAAOejnAEAAAAA86GBbuvdPJiAlRl3NKMes0J%2Bo9Q%3DNJ1JYEc0HoRHRPs9WedrEi7GFmG8AD4SPKLRJtvTuUBu8rJr62"
+access_token = "1386500592416133120-bjPwrMVnYNcCkXjBADwTekuoEgWggL"
+access_token_secret = "e162ttonSYOy0iHqsPuyYr23bKhoW3DdcIxXHgT33pnED"
+
+# auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+# auth.set_access_token(access_token, access_token_secret)
+# api = tweepy.API(auth, wait_on_rate_limit=True)
+
+# try:
+#     api.verify_credentials()
+#     print("Authentication Successful")
+# except:
+#     print("Authentication Error")
